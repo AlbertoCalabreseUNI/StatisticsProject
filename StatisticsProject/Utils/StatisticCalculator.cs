99d8cab6eaf9642680f7263cda81f03b2084e21e
Alberto Calabrese <calabrese.1709532@studenti.uniroma1.it>
2021-11-12 21:02:32 +0100

    Added all Statistic Formulas up until Covariance.

--- StatisticsProject/Utils/StatisticCalculator.cs
+++ StatisticsProject/Utils/StatisticCalculator.cs
@@ -29,95 +29,153 @@ namespace StatisticsProject.Utils
             return Mean;
         }
 
-        /* Variables: X = 0, Y = 1 */
-        public static double ComputeStandardDeviation(List<DataPoint> DataSet, int Variable = 0)
+        public static double ComputeMeanAtN(List<DataPoint> DataSet, int Variable = 0, int N = 1)
         {
-            //We need the Mean to compute Standard Deviation, we can just use the function we wrote above.
-            double Mean = ComputeOfflineMean(DataSet, Variable);
-            double SumOfDerivation = 0;
+            double TempSum = 0;
             if(Variable == 0)
             {
-                foreach(DataPoint DP in DataSet)
-                    SumOfDerivation += DP.X * DP.X;
-            }else if(Variable == 1)
-            {
-                foreach (DataPoint DP in DataSet)
-                    SumOfDerivation += DP.Y * DP.Y;
-            }
-            double SumOfDerivationAverage = SumOfDerivation / DataSet.Count();
-            double StandardDeviation = Math.Sqrt(SumOfDerivationAverage - (Mean * Mean));
-
-            return StandardDeviation;
-        }
-
-        /* Variables: X = 0, Y = 1 */
-        public static double ComputeVariance(List<DataPoint> DataSet, int Variable = 0)
-        {
-            double OldMean = 0;
-            double CurrentMean = 0;
-            double SS = 0;
-
-            if(Variable == 0)
-            {
-                for (int i = 0; i < DataSet.Count; i++)
+                for (int i = 0; i < N; i++)
                 {
-                    OldMean = CurrentMean;
-                    CurrentMean += (DataSet[i].X - OldMean) / (i + 1);
-                    SS = SS + (DataSet[i].X - CurrentMean) * (DataSet[i].X - OldMean);
+                    TempSum += DataSet[i].X;
                 }
             }
             else if(Variable == 1)
             {
-                for (int i = 0; i < DataSet.Count; i++)
+                for (int i = 0; i < N; i++)
                 {
-                    OldMean = CurrentMean;
-                    CurrentMean += (DataSet[i].Y - OldMean) / (i + 1);
-                    SS = SS + (DataSet[i].Y - CurrentMean) * (DataSet[i].Y - OldMean);
+                    TempSum += DataSet[i].Y;
                 }
             }
 
-            double Variance = SS / DataSet.Count;
+            double Mean = TempSum / N;
+            return Mean;
+        }
+
+        /* Variables: X = 0, Y = 1 */
+        public static double ComputeMedian(List<DataPoint> DataSet, int Variable = 0)
+        {
+            double Median = 0;
+            List<DataPoint> sorted;
+
+            if (Variable == 0)
+            {
+                sorted = DataSet.OrderBy(x => x.X).ToList();
+                if (sorted.Count() % 2 == 1)
+                    Median = sorted[sorted.Count() / 2].X;
+                else
+                    Median = (sorted[sorted.Count() / 2].X + sorted[sorted.Count() / 2 + 1].X) / 2;
+            }
+            else if(Variable == 1)
+            {
+                sorted = DataSet.OrderBy(x => x.Y).ToList();
+                if (sorted.Count() % 2 == 1)
+                    Median = sorted[sorted.Count() / 2].Y;
+                else
+                    Median = (sorted[sorted.Count() / 2].Y + sorted[sorted.Count() / 2 + 1].Y) / 2;
+            }
+            return Median;
+        }
+
+        /* Variables: X = 0, Y = 1 */
+        /* Variance is Standard Deviation squared */
+        public static double ComputeVariance(List<DataPoint> DataSet, int Variable = 0)
+        {
+            double Mean = ComputeOfflineMean(DataSet, Variable);
+            double SumOfDistances = 0;
+
+            if(Variable == 0)
+            {
+                foreach(DataPoint DP in DataSet)
+                {
+                    //We should take the absolute values of the subtractions but since the result is squared
+                    //it'll always be positive.
+                    SumOfDistances += (DP.X - Mean) * (DP.X - Mean);
+                }
+            }
+            else if(Variable == 1)
+            {
+                foreach (DataPoint DP in DataSet)
+                {
+                    //We should take the absolute values of the subtractions but since the result is squared
+                    //it'll always be positive.
+                    SumOfDistances += (DP.Y - Mean) * (DP.Y - Mean);
+                }
+            }
+
+            double Variance = (1 / DataSet.Count()) * SumOfDistances; 
             return Variance;
+        }
+
+        public static double ComputeVarianceAtStepN(List<DataPoint> DataSet, int Variable = 0, int N = 1)
+        {
+            double SumOfDistances = 0;
+            double Mean = ComputeMeanAtN(DataSet, Variable, N);
+            if(Variable == 0)
+            {
+                for(int i = 0; i < N; i++)
+                {
+                    SumOfDistances += (DataSet[i].X - Mean) * (DataSet[i].X - Mean);
+                }
+            }
+            else if(Variable == 1)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    SumOfDistances += (DataSet[i].X - Mean) * (DataSet[i].X - Mean);
+                }
+            }
+
+            double VarianceAtN = (1 / N) * SumOfDistances;
+            return VarianceAtN;
+        }
+
+        /* Variables: X = 0, Y = 1 */
+        public static double ComputeStandardDeviation(List<DataPoint> DataSet, int Variable = 0)
+        {
+            return Math.Sqrt(ComputeVariance(DataSet, Variable));
+        }
+
+        /* Variables: X = 0, Y = 1 */
+        /* Welford Algorithm */
+        public static double ComputeOnlineVariance(List<DataPoint> DataSet, int Variable = 0)
+        {
+            double Variance = 0;
+            if (Variable == 0)
+            {
+                for (int i = 2; i < DataSet.Count(); i++)
+                {
+                    Variance += (DataSet[i].X - ComputeMeanAtN(DataSet, Variable, i - 1)) * (DataSet[i].X - ComputeMeanAtN(DataSet, Variable, i));
+                }
+            }
+            else if (Variable == 1)
+            {
+                for (int i = 2; i < DataSet.Count(); i++)
+                {
+                    Variance += (DataSet[i].Y - ComputeMeanAtN(DataSet, Variable, i - 1)) * (DataSet[i].Y - ComputeMeanAtN(DataSet, Variable, i));
+                }
+            }
+
+            return Variance;
+        }
+
+        public static double ComputeCrossProduct(List<DataPoint> DataSet, int Index = 0)
+        {
+            double XMean = ComputeOfflineMean(DataSet, 0);
+            double YMean = ComputeOfflineMean(DataSet, 1);
+
+            double CrossProduct = (DataSet[Index].X - XMean) * (DataSet[Index].Y - YMean);
+            return CrossProduct;
         }
 
         public static double ComputeCovariance(List<DataPoint> DataSet)
         {
-            double SC = 0;
-            double OldXMean = 0;
-            double CurrentYMean = 0;
+            double TempCrossProductSum = 0;
+            for(int i = 0; i < DataSet.Count(); i++)
+                TempCrossProductSum += ComputeCrossProduct(DataSet, i);
 
-            for (int i = 0; i < DataSet.Count; i++)
-            {
-                CurrentYMean += (DataSet[i].Y - CurrentYMean) / (i + 1);
-                SC = SC + (DataSet[i].X - OldXMean) * (DataSet[i].Y - CurrentYMean);
-                OldXMean += (DataSet[i].X - OldXMean) / (i + 1);
-            }
-
-            double Covariance = SC / DataSet.Count;
+            double Covariance = (1 / DataSet.Count()) * TempCrossProductSum;
             return Covariance;
         }
-
-        /* Variables: X = 0, Y = 1 */
-        /*
-        public static double ComputeSumOfSquares(List<DataPoint> DataSet, int Variable = 0)
-        {
-            double SumOfSquares = 0.0;
-            if(Variable == 0)
-            {
-                foreach (DataPoint DP in DataSet)
-                    SumOfSquares += DP.X * DP.X;
-            }
-            else if(Variable == 1)
-            {
-                foreach (DataPoint DP in DataSet)
-                    SumOfSquares += DP.Y * DP.Y;
-            }
-
-            double average = average(values);
-            SumOfSquares -= average * average * values.length;
-            return SumOfSquares;
-        }
-        */
 
         /* Variables: X = 0, Y = 1 */
         public static double ComputeCorrelation(List<DataPoint> DataSet)
@@ -139,6 +197,7 @@ namespace StatisticsProject.Utils
         public static List<Point> ComputeLinearRegressionPoints(List<DataPoint> DataSet, int Variable = 0)
         {
             List<Point> PointsForLine = new List<Point>();
+
 
             return PointsForLine;
         }
